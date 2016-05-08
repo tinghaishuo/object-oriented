53b89803601baba1a3761da2a02aa34ccd674758
tinghaishuo <s38455@qq.com>
2016-05-08 15:08:13 +0800

    Update Print.cpp

--- Calculator/Calculator/Print.cpp
+++ Calculator/Calculator/Print.cpp
@@ -4,7 +4,7 @@ void Print::PrintData(queue<string> output)
 	while (! output.empty())
 	{
 		string strData = output.front();
-		cout<<strData<<endl;//打印队列首元素
-		output.pop();//删除队列首元素
+		cout<<strData<<endl;
+		output.pop();
 	}
 }
